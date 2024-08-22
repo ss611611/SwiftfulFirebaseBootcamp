9970986e8bf153b43184f91c2318215cae1ba94e
jackie <ss611611@gmail.com>
2024-08-22 10:32:24 +0800

    init commit

---
+++ SwiftfulFirebaseBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by Jackie Lu on 2024/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebaseBootcampApp.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by Jackie Lu on 2024/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
