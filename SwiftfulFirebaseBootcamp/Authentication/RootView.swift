700edf9af0771061e59aa9bc17c433a502413fdb
jackie <ss611611@gmail.com>
2024-09-10 15:07:14 +0800

    Sign In With Google

--- SwiftfulFirebaseBootcamp/Authentication/RootView.swift
+++ SwiftfulFirebaseBootcamp/Authentication/RootView.swift
@@ -12,8 +12,10 @@ struct RootView: View {
     @State private var showSignInView: Bool = false
     var body: some View {
         ZStack {
-            NavigationStack {
-                SettingsView(showSignInView: $showSignInView)
+            if !showSignInView {
+                NavigationStack {
+                    SettingsView(showSignInView: $showSignInView)
+                }
             }
         }
         .onAppear {
