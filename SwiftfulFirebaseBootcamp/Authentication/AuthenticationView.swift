2d968a9e2dd1cb0860e896e4a7f0a15a01a46f31
jackie <ss611611@gmail.com>
2024-09-11 14:48:51 +0800

    Sign In Anonymously

--- SwiftfulFirebaseBootcamp/Authentication/AuthenticationView.swift
+++ SwiftfulFirebaseBootcamp/Authentication/AuthenticationView.swift
@@ -12,7 +12,6 @@ import GoogleSignInSwift
 @MainActor
 final class AuthenticationViewModel: ObservableObject {
     
-    let signInAppleHelper = SignInAppleHelper()
     
     func signInGoogle() async throws {
         let helper = SignInGoogleHelper()
@@ -26,6 +25,10 @@ final class AuthenticationViewModel: ObservableObject {
         try await AuthenticationManager.shared.signInWithApple(tokens: tokens)
     }
     
+    func signInAnonymous() async throws {
+        try await AuthenticationManager.shared.signInAnonymous()
+    }
+    
 }
 
 struct AuthenticationView: View {
@@ -35,6 +38,25 @@ struct AuthenticationView: View {
     
     var body: some View {
         VStack {
+            
+            Button(action: {
+                Task {
+                    do {
+                        try await viewModel.signInAnonymous()
+                        showSignInView = false
+                    } catch {
+                        print(error)
+                    }
+                }
+            }, label: {
+                Text("Sign In Anonymously")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange)
+                    .cornerRadius(10)
+            })
             
             NavigationLink {
                 SignInEmailView(showSignInView: $showSignInView)
