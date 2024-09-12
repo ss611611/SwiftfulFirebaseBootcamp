accadf6e38d1ecfb54cbada7fc81bfdd322dfb4c
jackie <ss611611@gmail.com>
2024-09-12 17:59:43 +0800

    Firebase Firestore Maps and Arrays

--- SwiftfulFirebaseBootcamp/Core/Profile/ProfileView.swift
+++ SwiftfulFirebaseBootcamp/Core/Profile/ProfileView.swift
@@ -25,12 +25,54 @@ final class ProfileViewModel: ObservableObject {
             self.user = try await UserManager.shared.getUser(userId: user.userId)
         }
     }
+    
+    func addUserPreferences(text: String) {
+        guard let user else { return }
+        
+        Task {
+            try await UserManager.shared.addUserPreferences(userId: user.userId, preferences: text)
+            self.user = try await UserManager.shared.getUser(userId: user.userId)
+        }
+    }
+    
+    func removeUserPreferences(text: String) {
+        guard let user else { return }
+        
+        Task {
+            try await UserManager.shared.removeUserPreferences(userId: user.userId, preferences: text)
+            self.user = try await UserManager.shared.getUser(userId: user.userId)
+        }
+    }
+    
+    func addFavoriteMovie() {
+        guard let user else { return }
+        let movie = Movie(id: "1", title: "Avata 2", isPopular: true)
+        Task {
+            try await UserManager.shared.addFavoriteMovie(userId: user.userId, movie: movie)
+            self.user = try await UserManager.shared.getUser(userId: user.userId)
+        }
+    }
+    
+    func removeFavoriteMovie() {
+        guard let user else { return }
+        
+        Task {
+            try await UserManager.shared.removeFavoriteMovie(userId: user.userId)
+            self.user = try await UserManager.shared.getUser(userId: user.userId)
+        }
+    }
 }
 
 struct ProfileView: View {
     
     @StateObject private var viewModel = ProfileViewModel()
     @Binding var showSignInView: Bool
+    
+    let preferenceOptions: [String] = ["Sports", "Movies", "Books"]
+    
+    private func preferenceIsSelected(text: String) -> Bool {
+        viewModel.user?.preferences?.contains(text) == true
+    }
     
     var body: some View {
         List {
@@ -45,6 +87,36 @@ struct ProfileView: View {
                     viewModel.togglePremiumStatus()
                 } label: {
                     Text("User is premium: \((user.isPremium ?? false).description.capitalized)")
+                }
+                
+                VStack {
+                    HStack {
+                        ForEach(preferenceOptions, id: \.self) { string in
+                            Button(string) {
+                                if preferenceIsSelected(text: string) {
+                                    viewModel.removeUserPreferences(text: string)
+                                } else {
+                                    viewModel.addUserPreferences(text: string)
+                                }
+                            }
+                            .font(.headline)
+                            .buttonStyle(.borderedProminent)
+                            .tint(preferenceIsSelected(text: string) ? .green : .red)
+                        }
+                    }
+                    
+                    Text("User preferences: \((user.preferences ?? []).joined(separator: ", "))")
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                }
+                
+                Button {
+                    if user.favoriteMovie == nil {
+                        viewModel.addFavoriteMovie()
+                    } else {
+                        viewModel.removeFavoriteMovie()
+                    }
+                } label: {
+                    Text("Favorite Movie: \((user.favoriteMovie?.title ?? ""))")
                 }
             }
         }
@@ -67,6 +139,7 @@ struct ProfileView: View {
 
 #Preview {
     NavigationStack {
-        ProfileView(showSignInView: .constant(false))
+//        ProfileView(showSignInView: .constant(false))
+        RootView()
     }
 }
--- SwiftfulFirebaseBootcamp/Firestore/UserManager.swift
+++ SwiftfulFirebaseBootcamp/Firestore/UserManager.swift
@@ -8,6 +8,12 @@
 import Foundation
 import FirebaseFirestore
 
+struct Movie: Codable {
+    let id: String
+    let title: String
+    let isPopular: Bool
+}
+
 struct DBUser: Codable {
     let userId: String
     let isAnonymous: Bool?
@@ -15,6 +21,8 @@ struct DBUser: Codable {
     let photoUrl: String?
     let dateCreated: Date?
     let isPremium: Bool?
+    let preferences: [String]?
+    let favoriteMovie: Movie?
     
     init(auth: AuthDataResultModel) {
         self.userId = auth.uid
@@ -23,6 +31,8 @@ struct DBUser: Codable {
         self.photoUrl = auth.photoUrl
         self.dateCreated = Date()
         self.isPremium = false
+        self.preferences = nil
+        self.favoriteMovie = nil
     }
     
     init(
@@ -31,7 +41,9 @@ struct DBUser: Codable {
         email: String? = nil,
         photoUrl: String? = nil,
         dateCreated: Date? = nil,
-        isPremium: Bool? = nil
+        isPremium: Bool? = nil,
+        preferences: [String]? = nil,
+        favoriteMovie: Movie? = nil
     ) {
         self.userId = userId
         self.isAnonymous = isAnonymous
@@ -39,6 +51,8 @@ struct DBUser: Codable {
         self.photoUrl = photoUrl
         self.dateCreated = dateCreated
         self.isPremium = isPremium
+        self.preferences = preferences
+        self.favoriteMovie = favoriteMovie
     }
     
 //    func togglePremiumStstus() -> DBUser {
@@ -64,6 +78,8 @@ struct DBUser: Codable {
         case photoUrl = "photo_url"
         case dateCreated = "date_created"
         case isPremium = "user_premium"
+        case preferences = "preferences"
+        case favoriteMovie = "favorite_movie"
     }
     
     init(from decoder: any Decoder) throws {
@@ -74,6 +90,8 @@ struct DBUser: Codable {
         self.photoUrl = try container.decodeIfPresent(String.self, forKey: .photoUrl)
         self.dateCreated = try container.decodeIfPresent(Date.self, forKey: .dateCreated)
         self.isPremium = try container.decodeIfPresent(Bool.self, forKey: .isPremium)
+        self.preferences = try container.decodeIfPresent([String].self, forKey: .preferences)
+        self.favoriteMovie = try container.decodeIfPresent(Movie.self, forKey: .favoriteMovie)
     }
     
     func encode(to encoder: any Encoder) throws {
@@ -84,6 +102,8 @@ struct DBUser: Codable {
         try container.encodeIfPresent(self.photoUrl, forKey: .photoUrl)
         try container.encodeIfPresent(self.dateCreated, forKey: .dateCreated)
         try container.encodeIfPresent(self.isPremium, forKey: .isPremium)
+        try container.encodeIfPresent(self.preferences, forKey: .preferences)
+        try container.encodeIfPresent(self.favoriteMovie, forKey: .favoriteMovie)
     }
     
 }
@@ -99,17 +119,17 @@ final class UserManager {
         userCollection.document(userId)
     }
     
-//    private let encoder: Firestore.Encoder = {
-//        let encoder = Firestore.Encoder()
+    private let encoder: Firestore.Encoder = {
+        let encoder = Firestore.Encoder()
 //        encoder.keyEncodingStrategy = .convertToSnakeCase
-//        return encoder
-//    }()
-//    
-//    private let decoder: Firestore.Decoder = {
-//        let decoder = Firestore.Decoder()
+        return encoder
+    }()
+    
+    private let decoder: Firestore.Decoder = {
+        let decoder = Firestore.Decoder()
 //        decoder.keyDecodingStrategy = .convertFromSnakeCase
-//        return decoder
-//    }()
+        return decoder
+    }()
     
     func createdNewUser(user:DBUser) async throws {
         try userDocument(userId: user.userId).setData(from: user, merge: false)
@@ -160,5 +180,40 @@ final class UserManager {
         ]
         
         try await userDocument(userId: userId).updateData(data)
+    }
+    
+    func addUserPreferences(userId: String, preferences: String) async throws {
+        let data: [String:Any] = [
+            DBUser.CodingKeys.preferences.rawValue : FieldValue.arrayUnion([preferences])
+        ]
+        
+        try await userDocument(userId: userId).updateData(data)
+    }
+    
+    func removeUserPreferences(userId: String, preferences: String) async throws {
+        let data: [String:Any] = [
+            DBUser.CodingKeys.preferences.rawValue : FieldValue.arrayRemove([preferences])
+        ]
+        
+        try await userDocument(userId: userId).updateData(data)
+    }
+    
+    func addFavoriteMovie(userId: String, movie: Movie) async throws {
+        guard let data = try? encoder.encode(movie) else {
+            throw URLError(.badURL)
+        }
+        let dict: [String:Any] = [
+            DBUser.CodingKeys.favoriteMovie.rawValue : data
+        ]
+        
+        try await userDocument(userId: userId).updateData(dict)
+    }
+    
+    func removeFavoriteMovie(userId: String) async throws {
+        let data: [String:Any?] = [
+            DBUser.CodingKeys.favoriteMovie.rawValue : nil
+        ]
+        
+        try await userDocument(userId: userId).updateData(data as [AnyHashable : Any])
     }
 }
